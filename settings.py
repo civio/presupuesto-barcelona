37ba8e0ed89efc874d58355a46c3537855235af0
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-02-11 15:36:02 +0100

    Setup settings & add new variables

--- settings.py
+++ settings.py
@@ -13,8 +13,17 @@ FEATURED_PROGRAMMES = ['1710', '3321', '3261', '1630', '2410', '3263', '3340', '
 OVERVIEW_INCOME_NODES = [['11', '113'], '13', '42', '43', '45', '46', '30', '33', '34', '39']
 OVERVIEW_EXPENSE_NODES = ['23', '92', '13', '16', '15', '01', '33', '32', '43', '44', '91', '93', '17']
 
+# Show Payments section in menu & home options. Default: False.
+SHOW_PAYMENTS           = False
+
+# Show Tax Receipt section in menu & home options. Default: False.
+SHOW_TAX_RECEIPT        = False
+
+# Show Counties & Towns links in Policies section in menu & home options. Default: False.
+SHOW_COUNTIES_AND_TOWNS = False
+
 # Show an extra tab with institutional breakdown. Default: True.
-SHOW_INSTITUTIONAL_TAB = True
+SHOW_INSTITUTIONAL_TAB  = True
 
 # Show an extra tab with funding breakdown (only applicable to some budgets). Default: False.
 # SHOW_FUNDING_TAB = False
@@ -34,6 +43,15 @@ LANGUAGES = (
   ('ca', 'Catal&agrave;'),
   ('es-ES', 'Castellano')
 )
+
+# Setup Main Entity Web Url
+MAIN_ENTITY_WEB_URL     = 'http://ajuntament.barcelona.cat/es/'
+
+# Setup Main Entity Legal Url (if empty we hide the link)
+MAIN_ENTITY_LEGAL_URL   = 'http://ajuntament.barcelona.cat/es/aviso-legal'
+
+# External URL for Cookies Policy (if empty we use out template page/cookies.html)
+COOKIES_URL             = 'http://www.barcelona.cat/es/cookies.html'
 
 # Allow overriding of default treemap color scheme
 COLOR_SCALE = [ '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#e7969c', '#bcbd22', '#17becf' ]
