9ec1a5390162c79029f506656c232564f43e3d79
David Cabo <david.cabo@gmail.com>
2016-02-27 13:44:53 +0100

    Translate Sankey labels to english and spanish. Closes civio/presupuesto-management#146

--- settings.py
+++ settings.py
@@ -11,30 +11,140 @@ PAYMENTS_LOADER = 'BarcelonaPaymentsLoader'
 FEATURED_PROGRAMMES = ['1532', '2313', '3291', '3321', '3371', '4411', '1711', '1631', '1321', '4314']
 
 OVERVIEW_INCOME_NODES = [
-                          { 'label': 'Impost sobre béns immobles (IBI)', 'nodes': [['11', '113'], ['11', '114']] },
-                          { 'label': 'Resta d\'impostos locals propis (plusvàlua, IAE, circulació y ICIO)', 'nodes': [['11', '115'], ['11', '116'], '13', '29'] },
-                          { 'label': 'Impostos cedits (IRPF, IVA i especials)', 'nodes': ['10', '21', '22'] },
-                          { 'label': 'Taxes, preus públics i altres', 'nodes': ['30', '32', '33', '34', '35', '36', '38', '39'] },
-                          { 'label': 'Transferències corrents de l\'estat', 'nodes': '42' },
-                          { 'label': 'Transferències corrents de comunitats autònomes, ens locals i altres', 'nodes': ['41', '44', '45', '46', '47', '48', '49'] },
-                          { 'label': 'Rendiments patrimonials', 'nodes': ['50', '52', '53', '54', '55'] },
-                          { 'label': 'Ingressos de capital', 'nodes': ['60', '61', '68', '75', '79'] },
-                          { 'label': 'Ingressos financers', 'nodes': ['84', '90', '94'] }
+                          {
+                            'label.es': 'Impuesto sobre bienes inmuebles (IBI)',
+                            'label.en': 'Property tax (IBI)',
+                            'label.ca': 'Impost sobre béns immobles (IBI)',
+                            'nodes': [['11', '113'], ['11', '114']]
+                          },
+                          {
+                            'label.es': 'Otros impuestos locales propios (plusvalía, IAE, circulación y ICIO)',
+                            'label.en': 'Other own local taxes (capital gains, IAE, road tax and ICIO)',
+                            'label.ca': 'Resta d\'impostos locals propis (plusvàlua, IAE, circulació y ICIO)',
+                            'nodes': [['11', '115'], ['11', '116'], '13', '29']
+                          },
+                          {
+                            'label.es': 'Impuestos cedidos (IRPF, VAT y especiales)',
+                            'label.en': 'Transferred taxes (income tax, VAT and excises)',
+                            'label.ca': 'Impostos cedits (IRPF, IVA i especials)',
+                            'nodes': ['10', '21', '22']
+                          },
+                          {
+                            'label.es': 'Tasas, precios públicos y otros',
+                            'label.en': 'Fees, sales and other revenues',
+                            'label.ca': 'Taxes, preus públics i altres',
+                            'nodes': ['30', '32', '33', '34', '35', '36', '38', '39']
+                          },
+                          {
+                            'label.es': 'Transferencias corrientes del estado',
+                            'label.en': 'Current transfers from Spanish central government',
+                            'label.ca': 'Transferències corrents de l\'estat',
+                            'nodes': '42'
+                          },
+                          {
+                            'label.es': 'Transferencias corrientes de comunidades autónomas, entes locales y otros',
+                            'label.en': 'Current transfers from regions, local authorities and others',
+                            'label.ca': 'Transferències corrents de comunitats autònomes, ens locals i altres',
+                            'nodes': ['41', '44', '45', '46', '47', '48', '49']
+                          },
+                          {
+                            'label.es': 'Rendimientos patrimoniales',
+                            'label.en': 'Equity returns',
+                            'label.ca': 'Rendiments patrimonials',
+                            'nodes': ['50', '52', '53', '54', '55']
+                          },
+                          {
+                            'label.es': 'Ingresos de capital',
+                            'label.en': 'Capital inflows',
+                            'label.ca': 'Ingressos de capital',
+                            'nodes': ['60', '61', '68', '75', '79']
+                          },
+                          {
+                            'label.es': 'Ingresos financieros',
+                            'label.en': 'Financial income',
+                            'label.ca': 'Ingressos financers',
+                            'nodes': ['84', '90', '94']
+                          }
                         ]
 OVERVIEW_EXPENSE_NODES =  [
-                            { 'label': 'Deute', 'nodes': '01' },
-                            { 'label': 'Seguretat i mobilitat ciutadana', 'nodes': '13' },
-                            { 'label': 'Habitatge i urbanisme', 'nodes': ['15', '45'] },
-                            { 'label': 'Serveis urbans i medi ambient', 'nodes': ['16', '17'] },
-                            { 'label': 'Serveis socials i salut', 'nodes': ['23', '31'] },
-                            { 'label': 'Educació', 'nodes': '32' },
-                            { 'label': 'Cultura', 'nodes': '33' },
-                            { 'label': 'Esports', 'nodes': '34' },
-                            { 'label': 'Promoció econòmica i ocupació', 'nodes': ['43', ['49', '4931']] },
-                            { 'label': 'Transport públic', 'nodes': '44' },
-                            { 'label': 'Òrgans de govern', 'nodes': '91' },
-                            { 'label': 'Transferències a altres administracions', 'nodes': '94' },
-                            { 'label': 'Serveis de caràcter general i administració', 'nodes': ['21', '92', '93', ['49', '4911']] }
+                            {
+                              'label.es': 'Deuda',
+                              'label.en': 'Public debt',
+                              'label.ca': 'Deute',
+                              'nodes': '01'
+                            },
+                            {
+                              'label.es': 'Seguridad y mobilidad ciudadana',
+                              'label.en': 'Security and citizen mobility',
+                              'label.ca': 'Seguretat i mobilitat ciutadana',
+                              'nodes': '13'
+                            },
+                            {
+                              'label.es': 'Vivienda y urbanismo',
+                              'label.en': 'Housing and urban planning',
+                              'label.ca': 'Habitatge i urbanisme',
+                              'nodes': ['15', '45']
+                            },
+                            {
+                              'label.es': 'Servicios urbanos y medio ambiente',
+                              'label.en': 'Urban services and environment',
+                              'label.ca': 'Serveis urbans i medi ambient',
+                              'nodes': ['16', '17']
+                            },
+                            {
+                              'label.es': 'Servicios sociales y salud',
+                              'label.en': 'Social services and health',
+                              'label.ca': 'Serveis socials i salut',
+                              'nodes': ['23', '31']
+                            },
+                            {
+                              'label.es': 'Educación',
+                              'label.en': 'Education',
+                              'label.ca': 'Educació',
+                              'nodes': '32'
+                            },
+                            {
+                              'label.es': 'Cultura',
+                              'label.en': 'Culture',
+                              'label.ca': 'Cultura',
+                              'nodes': '33'
+                            },
+                            {
+                              'label.es': 'Deporte',
+                              'label.en': 'Sport',
+                              'label.ca': 'Esports',
+                              'nodes': '34'
+                            },
+                            {
+                              'label.es': 'Promoción económica y empleo',
+                              'label.en': 'Economic promotion and employment',
+                              'label.ca': 'Promoció econòmica i ocupació',
+                              'nodes': ['43', ['49', '4931']]
+                            },
+                            {
+                              'label.es': 'Transporte público',
+                              'label.en': 'Public transportation',
+                              'label.ca': 'Transport públic',
+                              'nodes': '44'
+                            },
+                            {
+                              'label.es': 'Órganos de gobierno',
+                              'label.en': 'Government bodies',
+                              'label.ca': 'Òrgans de govern',
+                              'nodes': '91'
+                            },
+                            {
+                              'label.es': 'Transferencias a otras administraciónes',
+                              'label.en': 'Transfers to other public administrations',
+                              'label.ca': 'Transferències a altres administracions',
+                              'nodes': '94'
+                            },
+                            {
+                              'label.es': 'Servicios de carácter general y administración',
+                              'label.en': 'General services and administration',
+                              'label.ca': 'Serveis de caràcter general i administració',
+                              'nodes': ['21', '92', '93', ['49', '4911']]
+                            }
                           ]
 
 # How aggresive should the Sankey diagram reorder the nodes. Default: 0.79 (Optional)
