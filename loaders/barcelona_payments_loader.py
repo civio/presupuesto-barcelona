399b44077d1f1a2e7234459dd8937544660d865b
David Cabo <david.cabo@gmail.com>
2016-05-11 05:49:29 +0200

    Use payment policy and programme ids to retrieve descriptions. Closes civio/presupuesto-management#233

--- loaders/barcelona_payments_loader.py
+++ loaders/barcelona_payments_loader.py
@@ -1,13 +1,23 @@
 # -*- coding: UTF-8 -*-
 from budget_app.loaders import PaymentsLoader
+from budget_app.models import Budget
 
 class BarcelonaPaymentsLoader(PaymentsLoader):
 
     # Parse an input line into fields
     def parse_item(self, budget, line):
+        # Instead of using the descriptions in the input file (which are in catalan),
+        # we use the area/policy and programme ids to fetch the descriptions in
+        # the language being loaded.
+        area_id = line[0].strip()
+        area = Budget.objects.get_all_descriptions(budget.entity)['functional'][area_id]
+
+        programme_id = line[2].strip()
+        programme = Budget.objects.get_all_descriptions(budget.entity)['functional'][programme_id]
+
         return {
-            'area': line[1].strip(),
-            'programme': line[3].strip(),
+            'area': area,
+            'programme': programme,
             'fc_code': None,  # We don't try (yet) to have foreign keys to existing records
             'ec_code': None,
             'date': None,
