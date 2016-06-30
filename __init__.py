8ead1ff3ade64c21099db3c3a89cbacb149f48d7
David Cabo <david.cabo@gmail.com>
2016-06-30 14:50:59 +0200

    Amend base URLs civio/presupuesto-management#236

--- __init__.py
+++ __init__.py
@@ -9,11 +9,11 @@ def reverse_decorator(func):
         # In production, rewrite URLs to use the prefixes provided by the client
         if hasattr(settings, 'USE_PRODUCTION_URLS') and settings.USE_PRODUCTION_URLS == True:
             if abs_path.startswith('/ca/'):
-                abs_path = '/estrategiaifinances/ca/pressupostobert' + abs_path[3:]
+                abs_path = '/estrategiaifinances/pressupostobert/ca' + abs_path[3:]
             elif abs_path.startswith('/en/'):
-                abs_path = '/estrategiaifinances/en/openbudget' + abs_path[3:]
+                abs_path = '/estrategiaifinances/pressupostobert/en' + abs_path[3:]
             elif abs_path.startswith('/es/'):
-                abs_path = '/estrategiaifinances/es/presupuestoabierto' + abs_path[3:]
+                abs_path = '/estrategiaifinances/pressupostobert/es' + abs_path[3:]
 
         return abs_path
     return inner
