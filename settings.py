5fbbb864b4d9ef8c3d5da67a6e9df850568bdd01
David Cabo <david.cabo@gmail.com>
2018-05-09 13:14:58 +0200

    Use 2016-based inflation, and add 2017 data civio/presupuesto-management#717

--- settings.py
+++ settings.py
@@ -240,7 +240,7 @@ DATA_SOURCE_BUDGET      = 'http://governobert.bcn.cat/estrategiaifinances/es/i-p
 DATA_SOURCE_POPULATION  = 'http://www.ine.es/jaxiT3/Tabla.htm?t=2861'
 
 # Setup Data Source Inflation link
-DATA_SOURCE_INFLATION   = 'http://www.ine.es/jaxiT3/Tabla.htm?t=10019&L=0'
+DATA_SOURCE_INFLATION   = 'http://www.ine.es/jaxiT3/Tabla.htm?t=22350&L=0'
 
 # Setup Main Entity Web Url
 MAIN_ENTITY_WEB_URL     = 'http://ajuntament.barcelona.cat/es/'
